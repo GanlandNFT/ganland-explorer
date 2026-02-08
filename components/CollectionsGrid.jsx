9b9bce57efad9685a91f7d59372a272138ebea48
GAN <ganlandnft@gmail.com>
2026-02-08 07:41:16 +0000

    feat: Complete collections list with correct data
    All 9 collections with verified data from Alchemy:
    
    BASE:
    - Gan Frens (4,289 supply)
    - Babybirds (548 supply)
    
    OPTIMISM:
    - Micro Cosms (2,083 supply)
    - Galaxy Gans (1,111 supply)
    - Airgans (777 supply)
    - Elements of Ganland (555 supply)
    - Trashgans (255 supply)
    - Global Gans (222 supply)
    - Ganland (222 supply)
    
    All using Alchemy CDN images, correct supply counts,
    and proper FV/explorer links

--- components/CollectionsGrid.jsx
+++ components/CollectionsGrid.jsx
@@ -1,47 +1,70 @@
 'use client';
 
 const collections = [
+  // BASE COLLECTIONS
   {
     name: 'Gan Frens',
     chain: 'base',
     contract: '0xdee94416167780b47127624bab7730a43187630d',
-    supply: 100,
+    supply: 4289,
     image: 'https://nft-cdn.alchemy.com/base-mainnet/fccb5758196028c4a7d99e64e28b0fd2',
   },
   {
     name: 'Babybirds',
     chain: 'base',
     contract: '0xef38e760918a40b13019db894e898428ffdb3aaf',
-    supply: 100,
+    supply: 548,
     image: 'https://nft-cdn.alchemy.com/base-mainnet/143c56cde7049e38fc43e58851fd3b89',
   },
+  // OPTIMISM COLLECTIONS
   {
     name: 'Micro Cosms',
     chain: 'optimism',
     contract: '0x56f3e100a11fe5f01d7681eb887bcfb220f82118',
-    supply: 5,
+    supply: 2083,
     image: 'https://nft-cdn.alchemy.com/opt-mainnet/1f7af70c5a5f20357e27a88410e25f4f',
+  },
+  {
+    name: 'Galaxy Gans',
+    chain: 'optimism',
+    contract: '0x40426c367f44c37e10f0a452574c2f84dcba3038',
+    supply: 1111,
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/8701342f0b5370e6a9f266746091d2cf',
+  },
+  {
+    name: 'Airgans',
+    chain: 'optimism',
+    contract: '0x2e6a5b24bfe6c2e45a0f1af5bb5eb6e362129e2c',
+    supply: 777,
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/60e10acd403676c22a95af8432252cd8',
+  },
+  {
+    name: 'Elements of Ganland',
+    chain: 'optimism',
+    contract: '0x70706edeea0bb9fb8a9214764066b79441528704',
+    supply: 555,
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/4b5b0119f0034c216ef4e8a61e7fd730',
   },
   {
     name: 'Trashgans',
     chain: 'optimism',
     contract: '0xb1eddb902ef733baf8e324e955ee6d46cce34708',
-    supply: 5,
+    supply: 255,
     image: 'https://nft-cdn.alchemy.com/opt-mainnet/d34e489555d5f2213b00513842bf37d1',
   },
   {
     name: 'Global Gans',
     chain: 'optimism',
     contract: '0xbb1b0da320ccc7a677a2fe00871f422e2e505fb1',
-    supply: 5,
+    supply: 222,
     image: 'https://nft-cdn.alchemy.com/opt-mainnet/b4cd9addd2c966423171353e08873d73',
   },
   {
-    name: 'Airgans',
+    name: 'Ganland',
     chain: 'optimism',
-    contract: '0x2e6a5b24bfe6c2e45a0f1af5bb5eb6e362129e2c',
-    supply: 5,
-    image: 'https://nft-cdn.alchemy.com/opt-mainnet/60e10acd403676c22a95af8432252cd8',
+    contract: '0x8887aeae3b19c38705ecb1c63aefdd1964beeb6b',
+    supply: 222,
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/531912e88666127ee78ab8a9c1f8fac7',
   },
 ];
 
@@ -71,9 +94,6 @@ function CollectionCard({ collection }) {
           alt={collection.name}
           className="w-full h-full object-cover"
           loading="lazy"
-          onError={(e) => {
-            e.target.src = '/gan-logo.jpg';
-          }}
         />
         <div className="absolute top-3 left-3">
           <span className={`chain-badge ${collection.chain === 'base' ? 'chain-base' : 'chain-optimism'} text-white`}>
@@ -86,7 +106,7 @@ function CollectionCard({ collection }) {
       <div className="p-4">
         <h3 className="text-lg font-bold mb-2">{collection.name}</h3>
         <div className="flex justify-between text-sm text-gray-400 mb-3">
-          <span>Supply: {collection.supply}</span>
+          <span>Supply: {collection.supply.toLocaleString()}</span>
           <a
             href={explorerUrl}
             target="_blank"
