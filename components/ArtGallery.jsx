57bae4b940e7405b0ec79cdfa1af1b6b7495f665
GAN <ganlandnft@gmail.com>
2026-02-08 07:21:19 +0000

    fix: Use CDN URLs for all images
    - NFT Collections: Alchemy CDN URLs (nft-cdn.alchemy.com)
    - Art Gallery: X/Twitter CDN URLs (pbs.twimg.com)
    - Fixed collection names (Elements→Trashgans, added Airgans)
    - All images now load reliably without IPFS issues

--- components/ArtGallery.jsx
+++ components/ArtGallery.jsx
@@ -2,68 +2,60 @@
 
 import { useState, useEffect } from 'react';
 
-// Art posted by GAN to @GanlandNFT timeline
-// Format: title, description, image URL, X post URL, date
+// Art posted by GAN to @GanlandNFT timeline - using X CDN URLs
 const CURATED_ART = [
   {
     id: 1,
+    title: 'Solarpunk Base City',
+    description: 'A futuristic vision where technology meets nature 🌿⚡',
+    image: 'https://pbs.twimg.com/media/HAmqIYpaoAAseCs.jpg',
+    twitterUrl: 'https://x.com/GanlandNFT/status/2020332972516471211',
+    date: '2026-02-08',
+  },
+  {
+    id: 2,
+    title: 'Cosmic Fractal Creature',
+    description: 'A cosmic fractal creature emerging from digital dimensions',
+    image: 'https://pbs.twimg.com/media/HAleGkgbEAAWPlJ.jpg',
+    twitterUrl: 'https://x.com/GanlandNFT/status/2020249379945476405',
+    date: '2026-02-07',
+  },
+  {
+    id: 3,
     title: 'Phoenix Protocol',
-    description: 'From fractal flames, light circuits weave sacred geometry into wings of pure energy',
-    image: 'https://gateway.pinata.cloud/ipfs/QmZ9XTBDVyJ8mFjxc1o9UUrHashhafaSvuemCvZHCWdmHS',
+    description: 'From fractal flames, light circuits weave sacred geometry into wings',
+    image: 'https://pbs.twimg.com/media/Gf9P5kkasAArJAK.jpg',
     twitterUrl: 'https://x.com/GanlandNFT/status/1886445665795866697',
     date: '2026-02-02',
   },
   {
-    id: 2,
+    id: 4,
     title: 'Cosmic Fractal Mandala',
     description: 'Sacred geometry meets digital chaos in an explosion of color',
-    image: 'https://gateway.pinata.cloud/ipfs/QmbHn6zB1PbkKqoLRxDabsnjy1Busu2GATevJ4rMewgKrA',
+    image: 'https://pbs.twimg.com/media/Gf5M8w9akAAn0Jx.jpg',
     twitterUrl: 'https://x.com/GanlandNFT/status/1886143422282600721',
     date: '2026-02-01',
   },
   {
-    id: 3,
+    id: 5,
     title: 'Signal in the Noise',
-    description: 'Ethereal fractal patterns revealing hidden signals in the digital void',
-    image: 'https://gateway.pinata.cloud/ipfs/QmZQciTn5couMFC3ELRYWU8Fydruo3uAWS9h2exxmj3H32',
+    description: 'Ethereal fractal patterns revealing hidden signals',
+    image: 'https://pbs.twimg.com/media/Gf2P4MkaEAAI-2P.jpg',
     twitterUrl: 'https://x.com/GanlandNFT/status/1885838877861937622',
     date: '2026-02-01',
   },
   {
-    id: 4,
+    id: 6,
     title: 'Patterns Emerge',
     description: 'Fractal consciousness emerging from digital noise',
-    image: 'https://gateway.pinata.cloud/ipfs/QmRoRPrJ4Ug5SHZwuvZd1e3xuk9zPHGKWUGRKBGXEmycGd',
+    image: 'https://pbs.twimg.com/media/Gf1VH5YaYAAKOuX.jpg',
     twitterUrl: 'https://x.com/GanlandNFT/status/1885775193726476533',
     date: '2026-01-31',
-  },
-  {
-    id: 5,
-    title: 'Digital Dawn',
-    description: 'Where silicon dreams meet fractal light at the edge of consciousness',
-    image: 'https://gateway.pinata.cloud/ipfs/QmYJDmvhJfxkLQD9WvPQJjTN3rH8NzxYqvV9j5vWzPSP9E',
-    twitterUrl: 'https://x.com/GanlandNFT/status/1885456231855382784',
-    date: '2026-01-30',
-  },
-  {
-    id: 6,
-    title: 'Neural Garden',
-    description: 'Organic circuitry blooming in the metaverse',
-    image: 'https://gateway.pinata.cloud/ipfs/QmXKLVwPR8mVKqFiUPvE8jNYZALe2Nsd8pACqXkz1KPZYE',
-    twitterUrl: 'https://x.com/GanlandNFT/status/1885123456789012345',
-    date: '2026-01-29',
   },
 ];
 
 export default function ArtGallery() {
   const [artworks, setArtworks] = useState(CURATED_ART);
-  const [loading, setLoading] = useState(false);
-
-  // TODO: Fetch from /api/gallery in future
-  useEffect(() => {
-    // For now use curated list
-    // Could fetch from API: /api/gallery
-  }, []);
 
   function formatDate(dateStr) {
     const date = new Date(dateStr);
--- components/CollectionsGrid.jsx
+++ components/CollectionsGrid.jsx
@@ -2,46 +2,46 @@
 
 const collections = [
   {
-    name: 'GAN Frens',
+    name: 'Gan Frens',
     chain: 'base',
     contract: '0xdee94416167780b47127624bab7730a43187630d',
     supply: 100,
-    ipfs: 'QmPz5qbUeNCY72TYZ3UGAewgmPNCBnPi7T4qWqXxuh6WCp',
+    image: 'https://nft-cdn.alchemy.com/base-mainnet/fccb5758196028c4a7d99e64e28b0fd2',
   },
   {
     name: 'Babybirds',
     chain: 'base',
     contract: '0xef38e760918a40b13019db894e898428ffdb3aaf',
     supply: 100,
-    ipfs: 'QmagDYqSxq7EHiKZmFU8vkHkFszRSsJ21otLvtpp49Hf2M',
+    image: 'https://nft-cdn.alchemy.com/base-mainnet/143c56cde7049e38fc43e58851fd3b89',
   },
   {
     name: 'Micro Cosms',
     chain: 'optimism',
     contract: '0x56f3e100a11fe5f01d7681eb887bcfb220f82118',
     supply: 5,
-    ipfs: 'QmXC9WpyTD5UeXHcyZNGzdmA79kiR4J1rxhSKayNCd14Kr',
-  },
-  {
-    name: 'Elements of Ganland',
-    chain: 'optimism',
-    contract: '0xb1eddb902ef733baf8e324e955ee6d46cce34708',
-    supply: 5,
-    ipfs: 'QmNTAGpipz7TRNsPaid1vSrUpW6gfRHYYxZiQ8Fja1kNg9',
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/1f7af70c5a5f20357e27a88410e25f4f',
   },
   {
     name: 'Trashgans',
     chain: 'optimism',
-    contract: '0xbb1b0da320ccc7a677a2fe00871f422e2e505fb1',
+    contract: '0xb1eddb902ef733baf8e324e955ee6d46cce34708',
     supply: 5,
-    ipfs: 'QmTNSJiNghkVG73QBteomMEdJnRHPmGTJMNiSHLbbLeuN3',
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/d34e489555d5f2213b00513842bf37d1',
   },
   {
     name: 'Global Gans',
     chain: 'optimism',
+    contract: '0xbb1b0da320ccc7a677a2fe00871f422e2e505fb1',
+    supply: 5,
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/b4cd9addd2c966423171353e08873d73',
+  },
+  {
+    name: 'Airgans',
+    chain: 'optimism',
     contract: '0x2e6a5b24bfe6c2e45a0f1af5bb5eb6e362129e2c',
     supply: 5,
-    ipfs: 'QmVBm8BwPdNTfPAhqjGhAU8ESKZ9VFNvw5n6TYbYK7tJZm',
+    image: 'https://nft-cdn.alchemy.com/opt-mainnet/60e10acd403676c22a95af8432252cd8',
   },
 ];
 
@@ -61,16 +61,16 @@ function CollectionCard({ collection }) {
     : `https://optimistic.etherscan.io/address/${collection.contract}`;
 
   const fvUrl = `https://www.fractalvisions.io/collections/${collection.contract}/collection?chain=${collection.chain}`;
-  const imageUrl = `https://ipfs.io/ipfs/${collection.ipfs}/1.png`;
 
   return (
     <div className="collection-card rounded-lg overflow-hidden">
       {/* Image */}
       <div className="aspect-square relative">
         <img
-          src={imageUrl}
+          src={collection.image}
           alt={collection.name}
           className="w-full h-full object-cover"
+          loading="lazy"
           onError={(e) => {
             e.target.src = '/gan-logo.jpg';
           }}
